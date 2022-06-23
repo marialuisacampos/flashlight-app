b96153d098b980441f04dc5184658b1cf0200bd1
marialuisacampos <luisa.campos@ufrpe.br>
2022-06-23 15:43:40 -0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
+//import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [toggle, setToggle] = React.useState(false)
+
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle)
+
+  React.useEffect(() => {
+    Alert.alert('Você pode chacoalhar ou clicar para ligar e desligar a lanterna.')
+  }, [])
+
+  React.useEffect(() => {
+    Torch.switchState(toggle);
+  }, [toggle])
+
+
+  React.useEffect(() => {
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle)
+    });
+
+    return () => subscription.remove();
+  }, [])
+
+  return(
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity onPress={handleChangeToggle}>
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff} 
+          source={toggle ? require('./assets/icons/eco-light.png') : require('./assets/icons/eco-light-off.png')}
+        />
+        <Image
+          style={style.dioLogo} 
+          source={toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png')}
+        />
+      </TouchableOpacity>
     </View>
-  );
-}
+  )
+};
 
-const styles = StyleSheet.create({
+export default App;
+
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightingOn: {
+    resizeMode: 'contain', //independente do tamanho, ela vai se adequar
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff: {
+    resizeMode: 'contain', //independente do tamanho, ela vai se adequar
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
+  },
+  dioLogo: {
+    resizeMode: 'contain', //independente do tamanho, ela vai se adequar
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
+  }
 });
